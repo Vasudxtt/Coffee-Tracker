522d57fbdba9b7493f28f7db1e8a5d34c1ec1378
Vasudxtt <141990594+Vasudxtt@users.noreply.github.com>
2025-07-23 00:11:51 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import CoffeeForm from "./components/CoffeeForm"
 import Hero from "./components/Hero"
 import Layout from "./components/Layout"
+import Stats from "./components/Stats"
 
 
 function App() {
@@ -9,6 +10,7 @@ const isAuthenticated = false
 
 const authenticatedContent = (
   <>
+  <Stats/>
   </>
 )
 
