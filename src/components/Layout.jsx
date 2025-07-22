12cf703f7b254d712e8a41170d80f6cdaeee12b2
Vasudxtt <141990594+Vasudxtt@users.noreply.github.com>
2025-07-23 00:03:20 +0530

    update

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,7 +1,16 @@
 export default function Layout(props){
   const {children} = props
+
+  const header = (
+    <header></header>
+  )
+
+  const footer = (
+    <footer></footer>
+  )
   return(
     <>
+    {header}
     </>
   )
 }
