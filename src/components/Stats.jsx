4dc32b671b10b902cbfc960b6c424d5c1538f9ae
Vasudxtt <141990594+Vasudxtt@users.noreply.github.com>
2025-07-26 02:32:59 +0530

    update

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -16,7 +16,8 @@ export default function Stats() {
         <h2>Stats</h2>
       </div>
       <div className="stats-grid">
-    <StatCard lg></StatCard>
+        {/* if we want we can write lg like this to as the value is been defined and if we write lg={true} then it is also fine */}
+    <StatCard lg title=""></StatCard>
     <StatCard></StatCard>
     <StatCard></StatCard>
     <StatCard></StatCard>
