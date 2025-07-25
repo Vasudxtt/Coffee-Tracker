03fc5fdcf60dfabf7b55fa36de30bb4c5d436f4d
Vasudxtt <141990594+Vasudxtt@users.noreply.github.com>
2025-07-26 01:15:22 +0530

    update

--- src/components/CoffeeForm.jsx
+++ src/components/CoffeeForm.jsx
@@ -13,10 +13,12 @@ export default function CoffeeForm() {
       {/* this div will have the 6 button of the drop down */}
       <div className="coffee-grid">
         {coffeeOptions.slice(0, 5).map((option, optionIndex) => {
+          return(
           <button className="button-card" key={optionIndex}>
             <h4>{option.name}</h4>
             <p>{option.caffeine}mg</p>
           </button>
+          )
         })}
         <button className="button-card">
           <h4>Other</h4>
