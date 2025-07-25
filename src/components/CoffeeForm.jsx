93404032752fb8adbf8ebb86796bffa28dd03151
Vasudxtt <141990594+Vasudxtt@users.noreply.github.com>
2025-07-26 01:07:34 +0530

    update

--- src/components/CoffeeForm.jsx
+++ src/components/CoffeeForm.jsx
@@ -6,6 +6,7 @@ export default function CoffeeForm() {
         <h2>Start Tracking Today</h2>
       </div>
        <h4>Select coffee type</h4>
+       {/* this div will have the 6 button of the drop down */}
             <div className="coffee-grid">
             </div>
     </>
