144cbb73c80b6096b53cbeb5d5c4c9f6e7cdc234
Vasudxtt <141990594+Vasudxtt@users.noreply.github.com>
2025-07-26 00:55:21 +0530

    update

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,7 +2,11 @@ export default function Layout(props){
   const {children} = props
 
   const header = (
-    <header></header>
+    <header>
+      <div>
+        <h1>CAFFIEND</h1>
+      </div>
+    </header>
   )
 
   const footer = (
