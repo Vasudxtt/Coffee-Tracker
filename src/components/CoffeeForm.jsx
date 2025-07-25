928d017a415c7deb2fadccccd1c8af988ee0d605
Vasudxtt <141990594+Vasudxtt@users.noreply.github.com>
2025-07-26 01:13:36 +0530

    update

--- src/components/CoffeeForm.jsx
+++ src/components/CoffeeForm.jsx
@@ -1,4 +1,4 @@
-import {coffeeOptions} from "../utils"
+import { coffeeOptions } from "../utils"
 
 
 
@@ -9,20 +9,20 @@ export default function CoffeeForm() {
         <i className="fa-solid fa-pencil" />
         <h2>Start Tracking Today</h2>
       </div>
-       <h4>Select coffee type</h4>
-       {/* this div will have the 6 button of the drop down */}
-            <div className="coffee-grid">
-              {coffeeOptions.slice(0,5).map((option,optionIndex)=>{
-                <button className="button-card" key={optionIndex}>
-                  <h4>{option.name}</h4>
-                  <p>{option.caffeine}mg</p>
-                </button>
-              })}
-              <button className="button-card">
-                <h4>Other</h4>
-                <p>n/a</p>
-              </button>
-            </div>
+      <h4>Select coffee type</h4>
+      {/* this div will have the 6 button of the drop down */}
+      <div className="coffee-grid">
+        {coffeeOptions.slice(0, 5).map((option, optionIndex) => {
+          <button className="button-card" key={optionIndex}>
+            <h4>{option.name}</h4>
+            <p>{option.caffeine}mg</p>
+          </button>
+        })}
+        <button className="button-card">
+          <h4>Other</h4>
+          <p>n/a</p>
+        </button>
+      </div>
     </>
   )
 }
