60698d21631bc7098fcebe5d8d3a97c26c4d51e5
Vasudxtt <141990594+Vasudxtt@users.noreply.github.com>
2025-07-26 01:05:51 +0530

    update

--- src/components/CoffeeForm.jsx
+++ src/components/CoffeeForm.jsx
@@ -1,6 +1,10 @@
-export default function CoffeeForm(){
-  return(
+export default function CoffeeForm() {
+  return (
     <>
+      <div className="section-header">
+        <i className="fa-solid fa-pencil" />
+        <h2>Start Tracking Today</h2>
+      </div>
     </>
   )
 }
