f760e68137aec92aeb51548a045e028914fb3c1d
Vasudxtt <141990594+Vasudxtt@users.noreply.github.com>
2025-07-26 02:34:23 +0530

    update

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -18,7 +18,7 @@ export default function Stats() {
       <div className="stats-grid">
         {/* if we want we can write lg like this to as the value is been defined and if we write lg={true} then it is also fine */}
     <StatCard lg title="Active Caffine Level"></StatCard>
-    <StatCard title=""></StatCard>
+    <StatCard title="Daily Caffine"></StatCard>
     <StatCard title=""></StatCard>
     <StatCard title=""></StatCard>
     <StatCard title=""></StatCard>
