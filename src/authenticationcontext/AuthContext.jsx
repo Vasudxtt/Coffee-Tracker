0427105ddc9cc5ffcb093cb77557d1d37dbc2474
Vasudxtt <141990594+Vasudxtt@users.noreply.github.com>
2025-07-27 01:58:03 +0530

    updating

--- src/authenticationcontext/AuthContext.jsx
+++ src/authenticationcontext/AuthContext.jsx
@@ -37,6 +37,9 @@ export function AuthProvider(props) {
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, async (user) => {
       // if there's no user, empty the user state and return from this listener
+
+      // if there is a user, then check if the user has data in the database, and if they do, then fetch said data and update the global state
+
     })
     return unsubscribe
   }, [])
