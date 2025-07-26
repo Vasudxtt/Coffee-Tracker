73d9ec897d55eb6c0e4d71ae1e8c489939d6ecf9
Vasudxtt <141990594+Vasudxtt@users.noreply.github.com>
2025-07-27 00:53:36 +0530

    update

--- src/components/Authentication.jsx
+++ src/components/Authentication.jsx
@@ -12,7 +12,7 @@ export default function Authentication() {
     <>
       <h2 className="sign-up-text">Sign Up / Login</h2>
       <p>Welcome to Coffee Tracker! Please sign up or login to continue.</p>
-      <input type="email" placeholder="Email" />
+      <input value={email} onChange={(e) => { setEmail(e.target.value) }} type="email" placeholder="Email" />
       <input type="password" placeholder="Password" />
       <button><p>Submit</p></button>
       {/* this is throwing a horizontal line to seprate it from the other */}
