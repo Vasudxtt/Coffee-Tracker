32c37dc2627b5fded0fe3778b70d631ea6d72ee3
Vasudxtt <141990594+Vasudxtt@users.noreply.github.com>
2025-07-27 01:44:35 +0530

    updating

--- src/authenticationcontext/AuthContext.jsx
+++ src/authenticationcontext/AuthContext.jsx
@@ -9,7 +9,7 @@ export function useAuth(){
 export function AuthProvider(props){
   const {children} = props
 const[user,setUser] = useState(null)
-
+const[globalData,setGlobalData] = useState(null)
 const value = {user}
 
 return(
