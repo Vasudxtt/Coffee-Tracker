d756af8f80fb1c24eb51be4afae158833f0c8075
Vasudxtt <141990594+Vasudxtt@users.noreply.github.com>
2025-07-27 02:43:45 +0530

    updating

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -16,7 +16,9 @@ export default function Layout(props){
         <h1 className="text-gradient">CAFFIEND</h1>
         <p>For Coffee Insatiates</p>
       </div>
-     {!globalUser && ( <button onClick={()=>{setShowModal(true)}}>
+     {globalUser ?  ( <button onClick={()=>{setShowModal(true)}}>
+        <p>Logout</p>
+      </button>):( <button onClick={()=>{setShowModal(true)}}>
         <p>Sign Up Free</p>
          <i className="fa-solid fa-mug-hot"></i>
       </button>)}
