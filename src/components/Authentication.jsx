32ba30243a207c219a0916cb7ef15b24aa197ddf
Vasudxtt <141990594+Vasudxtt@users.noreply.github.com>
2025-07-27 00:52:50 +0530

    update

--- src/components/Authentication.jsx
+++ src/components/Authentication.jsx
@@ -1,26 +1,26 @@
 import { useState } from "react"
 
-export default function Authentication(){
+export default function Authentication() {
 
- const [isRegistration, setIsRegistration] = useState(false)
- const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
-    const [isAuthenticating, setIsAuthenticating] = useState(false)
+  const [isRegistration, setIsRegistration] = useState(false)
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [isAuthenticating, setIsAuthenticating] = useState(false)
 
 
-  return(
+  return (
     <>
-    <h2 className="sign-up-text">Sign Up / Login</h2>
-    <p>Welcome to Coffee Tracker! Please sign up or login to continue.</p>
-    <input type="email" placeholder="Email"/>
-    <input type="password" placeholder="Password"/>
-    <button><p>Submit</p></button>
-     {/* this is throwing a horizontal line to seprate it from the other */}
-    <hr />
-    <div className="register-content">
-    <p>Don&apos;t have an account</p>
-    <button><p>Sign Up</p></button>
-    </div>
+      <h2 className="sign-up-text">Sign Up / Login</h2>
+      <p>Welcome to Coffee Tracker! Please sign up or login to continue.</p>
+      <input type="email" placeholder="Email" />
+      <input type="password" placeholder="Password" />
+      <button><p>Submit</p></button>
+      {/* this is throwing a horizontal line to seprate it from the other */}
+      <hr />
+      <div className="register-content">
+        <p>Don&apos;t have an account</p>
+        <button><p>Sign Up</p></button>
+      </div>
     </>
   )
 }
