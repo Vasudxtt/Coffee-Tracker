a1b8b4f0b585e7d153db7620a47d5651125c69a0
Vasudxtt <141990594+Vasudxtt@users.noreply.github.com>
2025-07-27 00:52:41 +0530

    update

--- src/components/Authentication.jsx
+++ src/components/Authentication.jsx
@@ -5,7 +5,7 @@ export default function Authentication(){
  const [isRegistration, setIsRegistration] = useState(false)
  const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
-    // const [isAuthenticating, setIsAuthenticating] = useState(false)
+    const [isAuthenticating, setIsAuthenticating] = useState(false)
 
 
   return(
