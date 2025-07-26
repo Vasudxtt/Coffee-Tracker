37d225486f7950f9be17263b361af547bba02049
Vasudxtt <141990594+Vasudxtt@users.noreply.github.com>
2025-07-27 01:47:45 +0530

    updating

--- src/authenticationcontext/AuthContext.jsx
+++ src/authenticationcontext/AuthContext.jsx
@@ -1,22 +1,26 @@
-import { createContext, useState,useEffect,useContext } from "react"
+import { createContext, useState, useEffect, useContext } from "react"
 // intializing the context
 const AuthContext = createContext()
 // creating a custom hook
-export function useAuth(){
+export function useAuth() {
   return useContext(AuthContext)
 }
 
-export function AuthProvider(props){
-  const {children} = props
-const[user,setUser] = useState(null)
-const[globalData,setGlobalData] = useState(null)
-const[isLoading,setIsLoading] = useState(false)
-const value = {user,globalData,setGlobalData,isLoading}
+export function AuthProvider(props) {
+  const { children } = props
+  const [user, setUser] = useState(null)
+  const [globalData, setGlobalData] = useState(null)
+  const [isLoading, setIsLoading] = useState(false)
+  const value = { user, globalData, setGlobalData, isLoading }
 
-return(
-  <AuthContext.Provider value={value}>
-    {children}
-  </AuthContext.Provider>
-)
+  function signup(email, password) {
+    return createUserWithEmailAndPassword(auth, email, password)
+  }
+
+  return (
+    <AuthContext.Provider value={value}>
+      {children}
+    </AuthContext.Provider>
+  )
 
 }
