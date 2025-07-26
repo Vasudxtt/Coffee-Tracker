07248ae6434eb77cb2de91696d9b946643af139a
Vasudxtt <141990594+Vasudxtt@users.noreply.github.com>
2025-07-27 02:54:41 +0530

    updating

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -3,12 +3,12 @@ import Authentication from "./Authentication"
 import Modal from "./Modal"
 import { useAuth } from "../authenticationcontext/AuthContext"
 
-export default function Layout(props){
-  const {children} = props
+export default function Layout(props) {
+  const { children } = props
 
-  const[showModal,setShowModal] = useState(false)
+  const [showModal, setShowModal] = useState(false)
 
-  const {globalUser , logout} = useAuth()
+  const { globalUser, logout } = useAuth()
 
   const header = (
     <header>
@@ -16,13 +16,13 @@ export default function Layout(props){
         <h1 className="text-gradient">CAFFIEND</h1>
         <p>For Coffee Insatiates</p>
       </div>
-     {globalUser ?  ( <button onClick={logout}>
+      {globalUser ? (<button onClick={logout}>
         <p>Logout</p>
-      </button>):( <button onClick={()=>{setShowModal(true)}}>
+      </button>) : (<button onClick={() => { setShowModal(true) }}>
         <p>Sign Up Free</p>
-         <i className="fa-solid fa-mug-hot"></i>
+        <i className="fa-solid fa-mug-hot"></i>
       </button>)}
-      </header>
+    </header>
   )
 
   const footer = (
@@ -30,20 +30,20 @@ export default function Layout(props){
       <p><span className="text-gradient">Caffiend</span> was made by <a target="_blank" href="https://www.vasuduttpareek.com">Vasudutt Pareek</a> <br />Using the <a href="https://www.fantacss.smoljames.com" target="_blank">FantaCSS</a> design library.<br />Check out the project on <a target="_black" href="https://github.com/Vasudxtt/Coffee-Tracker">GitHub</a>!</p>
     </footer>
   )
-function handleCloseModal() {
-        setShowModal(false)
-    }
+  function handleCloseModal() {
+    setShowModal(false)
+  }
 
-  return(
+  return (
     <>
-    {showModal && (<Modal handleCloseModal={()=>{setShowModal(false)}}>
-      <Authentication handleCloseModal={() => { setShowModal(false) }} />
-    </Modal>)}
-    {header}
-    <main>
-      {children}
-    </main>
-    {footer}
+      {showModal && (<Modal handleCloseModal={() => { setShowModal(false) }}>
+        <Authentication handleCloseModal={() => { setShowModal(false) }} />
+      </Modal>)}
+      {header}
+      <main>
+        {children}
+      </main>
+      {footer}
     </>
   )
 }
