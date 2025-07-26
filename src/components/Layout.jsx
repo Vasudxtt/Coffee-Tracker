d5c20b6514ed4d6e20a07be5903571bcd804b1f5
Vasudxtt <141990594+Vasudxtt@users.noreply.github.com>
2025-07-27 02:42:20 +0530

    updating

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,11 +1,14 @@
 import { useState } from "react"
 import Authentication from "./Authentication"
 import Modal from "./Modal"
+import { useAuth } from "../authenticationcontext/AuthContext"
 
 export default function Layout(props){
   const {children} = props
 
   const[showModal,setShowModal] = useState(false)
+
+  const {globalUser} = useAuth()
 
   const header = (
     <header>
@@ -13,7 +16,7 @@ export default function Layout(props){
         <h1 className="text-gradient">CAFFIEND</h1>
         <p>For Coffee Insatiates</p>
       </div>
-     {( <button onClick={()=>{setShowModal(true)}}>
+     {!globalUser && ( <button onClick={()=>{setShowModal(true)}}>
         <p>Sign Up Free</p>
          <i className="fa-solid fa-mug-hot"></i>
       </button>)}
