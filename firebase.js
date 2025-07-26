347514b61df2fdac33f8ca870a631d762f26572e
Vasudxtt <141990594+Vasudxtt@users.noreply.github.com>
2025-07-27 01:18:02 +0530

    updating

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD5yol2ze-FrSgwHz3cxo_wwS9xaA94dGM",
+  authDomain: "coffee-tracker-10.firebaseapp.com",
+  projectId: "coffee-tracker-10",
+  storageBucket: "coffee-tracker-10.firebasestorage.app",
+  messagingSenderId: "145880590531",
+  appId: "1:145880590531:web:070143de9ce1778ba5da76"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
