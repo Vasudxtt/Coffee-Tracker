a2cd643f5d41f7adcfb79f3a71936a5a7f62bd03
Vasudxtt <141990594+Vasudxtt@users.noreply.github.com>
2025-07-27 00:28:16 +0530

    update

--- src/components/Authentication.jsx
+++ src/components/Authentication.jsx
@@ -2,6 +2,7 @@ export default function Authentication(){
   return(
     <>
     <h2 className="sign-up-text">Sign Up / Login</h2>
+    <p>Welcome to Coffee Tracker! Please sign up or login to continue.</p>
     </>
   )
 }
