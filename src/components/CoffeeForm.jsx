f0ea97a1be9921a5607dd77780f03bd60cf96d14
Vasudxtt <141990594+Vasudxtt@users.noreply.github.com>
2025-07-27 00:44:23 +0530

    update

--- src/components/CoffeeForm.jsx
+++ src/components/CoffeeForm.jsx
@@ -19,7 +19,9 @@ const[min,setMin] = useState(0)
 
 // Now we will define a function here
 function handleSubmitForm(){
-  if(!isAuthenticated){}
+  if(!isAuthenticated){
+    return
+  }
 console.log(selectedCoffee,coffeeCost,hour,min);
 
 }
