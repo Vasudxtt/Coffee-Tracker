d3f48052a31644d19e8b1dfde73cf90f4935b3c8
Vasudxtt <141990594+Vasudxtt@users.noreply.github.com>
2025-07-27 03:14:17 +0530

    updating

--- src/components/CoffeeForm.jsx
+++ src/components/CoffeeForm.jsx
@@ -35,8 +35,8 @@ export default function
 
     // then we're going to create a new data object
     const newGlobalData = {
-// this is known as spreading out the existing global data inside this object 
-...globalData
+      // this is known as spreading out the existing global data inside this object 
+      ...globalData
     }
     // update the global state
 
