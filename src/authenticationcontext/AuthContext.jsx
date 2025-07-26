41cfe8764dd986f6c01e48b98abdbb7b0aca83df
Vasudxtt <141990594+Vasudxtt@users.noreply.github.com>
2025-07-27 01:56:54 +0530

    updating

--- src/authenticationcontext/AuthContext.jsx
+++ src/authenticationcontext/AuthContext.jsx
@@ -32,7 +32,7 @@ export function AuthProvider(props) {
     return signOut(auth)
   }
 
-  const value = { user, globalData, setGlobalData, isLoading,signup,login,logout }
+  const value = { globalUser, globalData, setGlobalData, isLoading,signup,login,logout }
 
   useEffect(()=>{
     const unsubscribe = onAuthStateChanged(auth,async()=>{})
