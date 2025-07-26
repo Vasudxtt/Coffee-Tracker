92140227257da85b13668957bb9e0b5bc50aec80
Vasudxtt <141990594+Vasudxtt@users.noreply.github.com>
2025-07-27 01:19:39 +0530

    updating

--- firebase.js
+++ firebase.js
@@ -5,13 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyD5yol2ze-FrSgwHz3cxo_wwS9xaA94dGM",
-  authDomain: "coffee-tracker-10.firebaseapp.com",
-  projectId: "coffee-tracker-10",
-  storageBucket: "coffee-tracker-10.firebasestorage.app",
-  messagingSenderId: "145880590531",
-  appId: "1:145880590531:web:070143de9ce1778ba5da76"
+    apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTHDOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECTID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGEBUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGINGSENDERID,
+    appId: import.meta.env.VITE_FIREBASE_APPID
 };
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
