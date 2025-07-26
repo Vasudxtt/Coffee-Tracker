297a69fd98876bfa562064b778ee32643136e766
Vasudxtt <141990594+Vasudxtt@users.noreply.github.com>
2025-07-27 00:56:36 +0530

    update

--- src/components/Authentication.jsx
+++ src/components/Authentication.jsx
@@ -14,7 +14,7 @@ async function handleAuthenticate() {
 
   return (
     <>
-      <h2 className="sign-up-text">Sign Up / Login</h2>
+       <h2 className="sign-up-text">{isRegistration ? 'Sign Up' : 'Login'}</h2>
       <p>Welcome to Coffee Tracker! Please sign up or login to continue.</p>
       <input value={email} onChange={(e) => { setEmail(e.target.value) }} type="email" placeholder="Email" />
       <input value={password} onChange={(e) => { setPassword(e.target.value) }} type="password" placeholder="Password" />
